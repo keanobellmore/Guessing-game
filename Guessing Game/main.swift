f2fb1f4cd8f5749de2aebf0161642f3d0ea2fda8
Keano Bellmore <keanobellmore@Interapts-MacBook-Pro.local>
2018-01-23 15:45:05 -0500

    first commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Keano Bellmore on 1/23/18.
+//  Copyright © 2018 Keano Bellmore. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
