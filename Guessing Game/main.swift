a21fae39eb15d14a657686e358e5a2d433ac8277
Keano Bellmore <keanobellmore@Interapts-MacBook-Pro.local>
2018-01-24 13:23:06 -0500

    outlined propject

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -7,6 +7,21 @@
 //
 
 import Foundation
+// 1.generate a random number
+// 2.ask user input
+// 3.take user unput
+// 4.determine wrong or right(loop until correct)
+// 5.if correct ask if they want to play again
+// 6.if incorrect tell them and then ask to play again
 
-print("Hello, World!")
+var randomNumber = arc4random_uniform(100)
+print("guess a random number")
+var input = readLine()
+var numInput = uint32(input!)!
+if randomNumber == numInput{
+ print("you're correct")
+
+
+} else  
+
 
